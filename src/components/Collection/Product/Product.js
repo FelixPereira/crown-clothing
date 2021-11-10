040aa58f3384a424801cec3afbf7891b8f50a6ae
FelixPereira <felixpereira538@gmail.com>
2021-11-10 07:23:01 +0200

    Adding style to products-

--- src/components/Collection/Product/Product.js
+++ src/components/Collection/Product/Product.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import './product.scss';
 
 const Product = ({ product }) => {
   return(
     <section className='collection'>
-      <h3 className='collection__title'>{ product.title }</h3>
-      {
-        product.items.map(item => (
-          <div className='collection__innerSection'>
+      <h2 className='collection__title'>{ product.title }</h2>
+      <div className='collection__innerSection'>
+        {
+          product.items.map(item => (
             <article key={item.id} className='collection__product'>
-            <img src={item.imageUrl} alt={product.items.name} width='250' />
-            <div>
-              <h4>{item.name}</h4>
-              <h4>{item.price}</h4>
-            </div>
-          </article>
-          </div>
-        ))
-      }
+              <img src={item.imageUrl} alt={product.items.name} width='250' height='300' />
+              <div className='product__info'>
+                <h4>{item.name}</h4>
+                <h4>{item.price}</h4>
+              </div>
+            </article>
+          ))
+        }
+      </div>
     </section>
   )
 }

2a6a5ef2b1b3133337d3c2c5e157d5662b333b30
FelixPereira <felixpereira538@gmail.com>
2021-11-14 03:23:40 +0200

    Creating the sign in component.

--- src/components/signIn/SignIn.js
+++ src/components/signIn/SignIn.js
@@ -4,11 +4,51 @@ class SignIn extends React.Component {
   constructor() {
     super();
 
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({email: '', password: ''});
+  }
+
+  handleChange = event => {
+    const {name, value} = event.target;
+
+    this.setState({ [name]: value });
+
   }
 
   render() {
     return(
-      <h1>Sign in</h1>
+      <div>
+        <h1>I already have an account</h1>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type='email'
+            name='email'
+            value={this.state.email}
+            required
+            onChange={this.handleChange} />
+          <label>Email</label>
+
+          <input 
+            type='password'
+            name='password'
+            value={this.state.password}
+            required
+            onChange={this.handleChange} />
+          <label>Password</label>
+
+          <input type='submit' value='Sign in' />
+        </form>
+      </div>
     )
   }
 }

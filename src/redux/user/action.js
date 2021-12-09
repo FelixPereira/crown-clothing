d0609110c12da62589be7fe9c2981b2696f151cb
FelixPereira <felixpereira538@gmail.com>
2021-12-09 23:32:06 +0200

    Creating the redux store

---
+++ src/redux/user/action.js
@@ -0,0 +1,14 @@
+const setCurrentUser = user => ({
+    type: 'SET_CURRENT_USER',
+    payload: user
+  })
+
+  const setCurrentUse = user => {
+    return(
+      {
+        type: 'SET_CURRENT_USER',
+        payload: user
+      }
+    )
+  }
+

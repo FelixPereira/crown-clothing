fefec7a8af7a6c77f9f1fe01a08a1f01e71a890c
Félix Pereira <felixpereira538@gmail.com>
2021-12-29 01:27:20 +0200

    Adding items to the cart dropdown

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../customButton/CustomButton';
+import CartItem from '../cartItems/cartItems';
 
 import './cart-dropdown.scss';
 
-const CartDropdown = () => {
+const CartDropdown = ({cartItems}) => {
   return(
     <div className='cart-dropdown'>
-      <div className='cart-items' />
+      <div className='cart-items'>
+        {
+          cartItems.map(cartItem => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        }
+      </div>
+      
       <CustomButton>Got to checkout</CustomButton>
     </div>
   )
 }
 
-export default CartDropdown;
+const mapStateToProps = ({cart: {cartItems}}) => ({
+  cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/redux/cart/cart-utilities.js
+++ src/redux/cart/cart-utilities.js
@@ -1,10 +1,16 @@
 export const addItemToCart = (itemToAdd, cartItems) => {
-  const existingItem = cartItems.find(cartItem => cartItem.id === itemToAdd.id);
+  const existingItem = cartItems.find(
+      cartItem => cartItem.id === itemToAdd.id
+    );
+
+
 
   if(existingItem) {
-    return cartItems.map(cartItem => cartItem.id === itemToAdd.id 
+    return cartItems.map(
+      cartItem => cartItem.id === itemToAdd.id 
       ? {...cartItem, quantity: cartItem.quantity + 1} 
-      : cartItem);
+      : cartItem
+      );
   }
 
   return [...cartItems, {...itemToAdd, quantity: 1}]
